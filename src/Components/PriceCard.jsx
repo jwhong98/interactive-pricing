000f87aa3c86500d5907cebf44bd517bd596adfd
jwhong <jwhong98@gmail.com>
2021-12-09 13:21:34 -0800

    completed mobile view

--- src/Components/PriceCard.jsx
+++ src/Components/PriceCard.jsx
@@ -1,27 +1,35 @@
 import React from 'react'
+import Switch from '@mui/material/Switch';
+import img from '../images/icon-check.svg'
+import Slider from '@mui/material/Slider';
 
 const PriceCard = () => {
     return (
         <div className="price-container">
             <div className="top-wrap">
-                <h3 className="subtitle">100k pageviews</h3>
-                <div className="slider"></div>
+                <h3 id="price-header">100k pageviews</h3>
+                <div className="slider-wrap">
+                    <Slider  
+                        defaultValue={50}
+                        
+                    />
+                </div>
                 <div className="price-wrap">
                     <h1 className="price">$16.00</h1>
                     <p className="label">/ month</p>
                 </div>
                 <div className="button-wrap">
-                    <p className="subtitle">Monthly Billing</p>
-                    <button type="button"></button>
-                    <p className="subtitle">Yearly Billing</p>
-                    <p className="discount"></p>
+                    <p className="label">Monthly Billing</p>
+                    <Switch />
+                    <p className="label">Yearly Billing</p>
+                    <span className="discount">-25%</span>
                 </div>
             </div>
             <div className="bottom-wrap">
                 <ul className="features">
-                    <li className="feature-item">Unlimited websites</li>
-                    <li className="feature-item">100% data ownership</li>
-                    <li className="feature-item">Email reports</li>
+                    <li className="feature-item"><img className="icon" alt="checkmark" src={img}/>Unlimited websites</li>
+                    <li className="feature-item"><img className="icon" alt="checkmark" src={img} />100% data ownership</li>
+                    <li className="feature-item"><img className="icon" alt="checkmark" src={img} />Email reports</li>
                 </ul>
                 <button className="trial" type="button">Start my trial</button>
             </div>
